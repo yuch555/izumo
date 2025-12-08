a4dba2e54f67f916e84fa427c7f38243b674efea
yamaneyuuta <yutayamane4889@gmail.com>
2025-12-08 09:16:43 +0900

    feat: FiscalPageにBackToHomeコンポーネントを追加

--- app/app/fiscal/page.tsx
+++ app/app/fiscal/page.tsx
@@ -1,8 +1,10 @@
+import { BackToHome } from "@/components/base/BackToHome";
 import { FiscalStats } from "../../src/components/features/FiscalStats";
 
 export default function FiscalPage() {
   return (
     <div className="container mx-auto p-4">
+      <BackToHome />
       <FiscalStats />
     </div>
   );
