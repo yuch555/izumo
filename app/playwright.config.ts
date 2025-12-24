ae1086f9983d48c3d8f88129bd75a81298156c2a
yamaneyuuta <yutayamane4889@gmail.com>
2025-12-25 06:27:53 +0900

    修正

--- app/playwright.config.ts
+++ app/playwright.config.ts
@@ -39,15 +39,17 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    ...(process.env.CI ? [] : [
+      {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+      },
+    ]),
 
     /* Test against mobile viewports. */
     // {
